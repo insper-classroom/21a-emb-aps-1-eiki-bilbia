d2ef979584e9f143cb5b2526653c1d51e0935d3e
Bilbia <biamcs2000@gmail.com>
2021-03-15 18:11:28 -0300

    botao pause com interrupt

--- src/main.c
+++ src/main.c
@@ -124,6 +124,7 @@ typedef struct{
 /* variaveis globais                                                    */
 /************************************************************************/
 
+volatile int pause_flag = 1;
 
 /************************************************************************/
 /* prototypes                                                           */
@@ -143,16 +144,74 @@ void play(song curr_song);
 /* interrupcoes                                                         */
 /************************************************************************/
 
+void but_callback(void){
+ 	pause_flag = !pause_flag;
+}
 
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
 
+void init(void)
+{
+	sysclk_init();
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	
+	// abrindo todos os PIOs
+	pmc_enable_periph_clk(ID_PIOA);
+	pmc_enable_periph_clk(ID_PIOB);
+	pmc_enable_periph_clk(ID_PIOC);
+	pmc_enable_periph_clk(ID_PIOD);
+	
+	// configurando but e led da placa
+	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	
+
+	// configurando leds da OLED1
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+	
+	// configurando buts da OLED1
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+
+	// configurando buzzer
+	pio_set_output(BUZZ_PIO, BUZZ_PIO_IDX_MASK, 0, 0, 0);
+	
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	// interrupt pra pausar
+	pio_handler_set(BUT_PIO,
+	BUT_PIO_ID,
+	BUT_PIO_IDX_MASK,
+	PIO_IT_FALL_EDGE,
+	but_callback);
+	
+	
+	
+	
+	NVIC_SetPriority(BUT_PIO_ID, 4); // Prioridade 4
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	
+	pio_enable_interrupt(BUT_PIO, BUT_PIO_IDX_MASK);
+}
+
 
 void tone(int freq, int time) {
+	while(pause_flag==1){
+		pio_clear(PIOA, LED1_PIO_IDX_MASK);
+		pio_clear(PIOC, LED2_PIO_IDX_MASK);
+		pio_clear(PIOB, LED3_PIO_IDX_MASK);
+		gfx_mono_draw_string("    PAUSE    ", 0,16, &sysfont);
+	}
 	// caso a nota seja uma pausa
 	if (freq == 0) {
-		gfx_mono_draw_string("                 ", 0,16, &sysfont);
+		gfx_mono_draw_string("             ", 0,16, &sysfont);
 		pio_clear(PIOD, BUZZ_PIO_IDX_MASK);
 		// pausa pelo tempo inserido
 		delay_ms(time);
@@ -160,7 +219,7 @@ void tone(int freq, int time) {
 	else {
 		char snum[5];
 		itoa(freq, snum, 10);
-		gfx_mono_draw_string("                 ", 10,50, &sysfont);
+		gfx_mono_draw_string("             ", 10,50, &sysfont);
 		gfx_mono_draw_string(snum, 50,16, &sysfont);
 		if(freq<350){pio_clear(PIOA, LED1_PIO_IDX_MASK);}
 		else if(freq<550){pio_clear(PIOC, LED2_PIO_IDX_MASK);}
@@ -179,6 +238,8 @@ void tone(int freq, int time) {
 		pio_set(PIOA, LED1_PIO_IDX_MASK);
 		pio_set(PIOC, LED2_PIO_IDX_MASK);
 		pio_set(PIOB, LED3_PIO_IDX_MASK);
+		
+		
 	}
 }
 
@@ -222,71 +283,15 @@ void play(song curr_song) {
 			noteDuration *= 1.5;
 		}
 
-		// permite pausar a música entre notas
-		int pause = 0;
-		if (!pio_get(PIOA, PIO_INPUT, BUT3_PIO_IDX_MASK)){
-			thisNote = notes * 2;
-			gfx_mono_draw_string("    STOP     ", 0,16, &sysfont);
-			delay_ms(500);
-			pio_clear(PIOB, LED3_PIO_IDX_MASK);
-			delay_ms(1000);
-			gfx_mono_draw_string("                 ", 0,16, &sysfont);
-		}
-		if(!pio_get(PIOC, PIO_INPUT, BUT2_PIO_IDX_MASK)){
-			pause=1;
-			pio_clear(PIOA, LED1_PIO_IDX_MASK);
-			pio_clear(PIOC, LED2_PIO_IDX_MASK);
-			pio_clear(PIOB, LED3_PIO_IDX_MASK);
-			gfx_mono_draw_string("    PAUSE    ", 0,16, &sysfont);
-			delay_ms(1000);
-			while(pause == 1){
-				if(!pio_get(PIOC, PIO_INPUT, BUT2_PIO_IDX_MASK)){
-					pio_set(PIOA, LED1_PIO_IDX_MASK);
-					pio_set(PIOC, LED2_PIO_IDX_MASK);
-					pio_set(PIOB, LED3_PIO_IDX_MASK);
-					pause = 0;
-				}}}
-
-				// toca a nota
-				tone(melody[thisNote], noteDuration * 0.9);
-				// breve pausa para poder diferenciar entre notas
-				delay_ms(noteDuration*0.1);
+		// toca a nota
+		tone(melody[thisNote], noteDuration * 0.9);
+		// breve pausa para poder diferenciar entre notas
+		delay_ms(noteDuration*0.1);
 				
 			}
 		}
 
-		void init(void)
-		{
-			sysclk_init();
-			WDT->WDT_MR = WDT_MR_WDDIS;
-			
-			// abrindo todos os PIOs
-			pmc_enable_periph_clk(ID_PIOA);
-			pmc_enable_periph_clk(ID_PIOB);
-			pmc_enable_periph_clk(ID_PIOC);
-			pmc_enable_periph_clk(ID_PIOD);
-			
-			// configurando but e led da placa
-			pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
-			pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
-			pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
-
-			// configurando leds da OLED1
-			pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
-			pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
-			pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
-			
-			// configurando buts da OLED1
-			pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
-			pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
-			pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
-			pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
-			pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
-			pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
-
-			// configurando buzzer
-			pio_set_output(BUZZ_PIO, BUZZ_PIO_IDX_MASK, 0, 0, 0);
-		}
+		
 
 
 		/************************************************************************/
@@ -296,9 +301,9 @@ void play(song curr_song) {
 		// Funcao principal chamada na inicalizacao do uC.
 		int main(void)
 		{
-			init();
-			board_init();
 			sysclk_init();
+			board_init();
+			init();
 			delay_init();
 			gfx_mono_ssd1306_init();
 			gfx_mono_draw_string("APS 1", 50,16, &sysfont);
@@ -310,22 +315,22 @@ void play(song curr_song) {
 			strcpy( got.title, got_title);
 			got.tempo = got_tempo;
 			got.melody = &got_melody;
-			got.size = sizeof(got_melody);
+			got.size = sizeof(got_melody)/sizeof(got_melody[0]);
 
 			strcpy( mii.title, mii_title);
 			mii.tempo = mii_tempo;
 			mii.melody = &mii_melody;
-			mii.size = sizeof(mii_melody);
+			mii.size = sizeof(mii_melody)/sizeof(mii_melody[0]);
 
 			strcpy( mario.title, mario_title);
 			mario.tempo = mario_tempo;
 			mario.melody = &mario_melody;
-			mario.size = sizeof(mario_melody);
+			mario.size = sizeof(mario_melody)/sizeof(mario_melody[0]);
 			
 			strcpy( starwars.title, starwars_title);
 			starwars.tempo = starwars_tempo;
 			starwars.melody = &starwars_melody;
-			starwars.size = sizeof(starwars_melody);
+			starwars.size = sizeof(starwars_melody)/sizeof(starwars_melody[0]);
 
 			disc disc1;
 			disc1.songs[0]= got;
@@ -335,15 +340,13 @@ void play(song curr_song) {
 			disc1.curr_song = disc1.songs[contador];
 			
 			while (1){
+				//-------------------------------------------------------------------------------
 				gfx_mono_draw_string(disc1.curr_song.title, 0,16, &sysfont);
 				pio_set(PIOC, LED_PIO_IDX_MASK);
 				pio_set(PIOA, LED1_PIO_IDX_MASK);
 				pio_set(PIOC, LED2_PIO_IDX_MASK);
 				pio_set(PIOB, LED3_PIO_IDX_MASK);
-				
-				//-------------------------------------------------------------------------------
-				
-				
+					
 				//-------------------------------------------------------------------------------
 				if (!pio_get(PIOD, PIO_INPUT, BUT1_PIO_IDX_MASK)){
 					back_song(&disc1);
@@ -360,10 +363,10 @@ void play(song curr_song) {
 					delay_ms(500);
 					pio_clear(PIOC, LED2_PIO_IDX_MASK);
 					delay_ms(500);
-					gfx_mono_draw_string("                 ", 0,16, &sysfont);
+					gfx_mono_draw_string("             ", 0,16, &sysfont);
 					play(disc1.curr_song);
 				}
-				
+					
 				//-------------------------------------------------------------------------------
 				if (!pio_get(PIOA, PIO_INPUT, BUT3_PIO_IDX_MASK)){
 					next_song(&disc1);
@@ -372,9 +375,6 @@ void play(song curr_song) {
 					pio_clear(PIOB, LED3_PIO_IDX_MASK);
 					delay_ms(500);
 				}
-				
-				
-
 			}
 			return 0;
 		}
